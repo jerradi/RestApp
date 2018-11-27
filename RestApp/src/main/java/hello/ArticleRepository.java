b2d1a934f3f9b0b3914c939af30816cd0a682335
Jim <jamal.radi.90@gmail.com>
2018-11-27 11:32:08 +0100

    introducing a web controller

--- RestApp/src/main/java/hello/ArticleRepository.java
+++ RestApp/src/main/java/hello/ArticleRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -14,7 +13,9 @@ public interface ArticleRepository extends PagingAndSortingRepository<Article, S
 
 	public Page<Book> findByName(@Param("name") String name, Pageable page);
 	 
+	
 	public List<Article> findByPrice(int price);
+	
 	public List<Article> findByMessage(String message, Pageable page);
 }
 
